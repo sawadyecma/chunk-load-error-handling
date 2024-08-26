49cc6529e7b094da55f0f4f3725319ac2a742514
sawadyecma <snowslidez@yahoo.co.jp>
2024-08-26 21:48:06 +1000

    refacctor: extract EachRouteWrapper

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
+import React, { ReactNode } from "react";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
 import { PATHS } from "./path";
 import { lazy } from "react";
 import { AppErrorBoundary } from "../basis/ErrorBoundary";
@@ -9,45 +9,38 @@ const ProductsPage = lazy(() => import("../pages/ProductsPage"));
 const ProductPage = lazy(() => import("../pages/ProductPage"));
 const CartPage = lazy(() => import("../pages/CartPage"));
 
-export const router = createBrowserRouter([
+const routeObjects: RouteObject[] = [
   {
     path: PATHS.home,
-    element: (
-      <AppErrorBoundary>
-        <ProductsPage />
-      </AppErrorBoundary>
-    ),
+    element: <ProductsPage />,
   },
   {
     path: PATHS.products,
-    element: (
-      <AppErrorBoundary>
-        <ProductsPage />
-      </AppErrorBoundary>
-    ),
+    element: <ProductsPage />,
   },
   {
     path: PATHS.productDetail,
-    element: (
-      <AppErrorBoundary>
-        <ProductPage />
-      </AppErrorBoundary>
-    ),
+    element: <ProductPage />,
   },
   {
     path: PATHS.cart,
-    element: (
-      <AppErrorBoundary>
-        <CartPage />
-      </AppErrorBoundary>
-    ),
+    element: <CartPage />,
   },
   {
     path: PATHS.profile,
-    element: (
-      <AppErrorBoundary>
-        <ProfilePage />
-      </AppErrorBoundary>
-    ),
+    element: <ProfilePage />,
   },
-]);
+];
+
+const EachRouteWrapper = ({ children }: { children: ReactNode }) => {
+  return <AppErrorBoundary>{children}</AppErrorBoundary>;
+};
+
+export const router = createBrowserRouter(
+  routeObjects.map((route) => {
+    const { element } = route;
+    if (!element) return route;
+    const wrapped = <EachRouteWrapper>{route.element}</EachRouteWrapper>;
+    return { ...route, element: wrapped };
+  })
+);
