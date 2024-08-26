ba315d5e895bb863b754e023156d4acc7f6a54bb
sawadyecma <snowslidez@yahoo.co.jp>
2024-08-26 19:44:13 +1000

    feat: chunk catch error

--- src/basis/ErrorBoundary.tsx
+++ src/basis/ErrorBoundary.tsx
@@ -4,13 +4,21 @@ import { ErrorBoundary } from "react-error-boundary";
 
 export const AppErrorBoundary = ({ children }: { children: ReactNode }) => (
   <ErrorBoundary
-    fallbackRender={({ error, resetErrorBoundary }) => (
-      <div>
-        <h1>Something went wrong</h1>
-        <pre>{error.message}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    )}
+    fallbackRender={({ error, resetErrorBoundary }) => {
+      console.log({ error });
+      if (error instanceof Error && error.name === "ChunkLoadError") {
+        return <h1>ChunkLoadError</h1>;
+      }
+
+      debugger;
+      return (
+        <div>
+          <h1>Something went wrong</h1>
+          <pre>{error.message}</pre>
+          <button onClick={resetErrorBoundary}>Try again</button>
+        </div>
+      );
+    }}
   >
     {children}
   </ErrorBoundary>
